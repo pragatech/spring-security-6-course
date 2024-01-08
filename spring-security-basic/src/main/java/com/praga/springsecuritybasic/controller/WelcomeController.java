9046d914237022be44fa42f8d190c135956e3f55
praga_xformics <pragatheeswaran.c@in.xformics.com>
2024-01-08 22:51:48 +0530

    spring boot app without spring security

---
+++ spring-security-basic/src/main/java/com/praga/springsecuritybasic/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.praga.springsecuritybasic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/")
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome(){
+        return "Welcome to Spring boot without security";
+    }
+}
